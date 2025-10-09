c5c7a4389e28685ed4fffe993bfe6ad3557d9046
koval <kovalartem9010@gmail.com>
2025-10-09 21:36:41 +0300

    Added service layer

---
+++ LibaryApp/LibraryApp.Business/Services/ILibraryService.cs
@@ -0,0 +1,13 @@
+﻿using LibraryApp.Business.DTOs;
+using LibraryApp.Data.Models;
+
+namespace LibraryApp.Business.Services;
+
+public interface ILibraryService
+{
+    List<BookListDTO> GetAllBooks();
+    List<BookStatusDto> GetAvailableBooks();
+    List<BookStatusDto> GetBorrowedBooks();
+    bool BorrowBook(Guid bookId);
+    bool ReturnBook(Guid bookId);
+}
