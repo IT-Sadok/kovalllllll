2dd8ba562560afc7e404b3256c45ba3be77cc04f
koval <kovalartem9010@gmail.com>
2025-10-09 21:35:45 +0300

    Add main model and DTO

---
+++ LibaryApp/LibraryApp.Data/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace LibraryApp.Data.Models;
+
+public class Book
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string YearOfPublication { get; set; }
+
+    // Consider using enum for statuses
+    public BookStatus Status { get; set; }
+}
+
+public enum BookStatus
+{
+    Available,
+    Borrowed
+}
