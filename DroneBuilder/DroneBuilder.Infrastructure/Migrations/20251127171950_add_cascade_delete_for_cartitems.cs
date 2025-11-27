9ca73d13b1a3d0f40bdb55f3a9458917820827fc
koval <kovalartem9010@gmail.com>
2025-11-27 19:21:22 +0200

    Update cart item: add cascade delete for product

---
+++ DroneBuilder/DroneBuilder.Infrastructure/Migrations/20251127171950_add_cascade_delete_for_cartitems.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DroneBuilder.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_cascade_delete_for_cartitems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Products_ProductId",
+                table: "CartItems");
+
+            migrationBuilder.UpdateData(
+                table: "Warehouses",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 11, 27, 17, 19, 49, 414, DateTimeKind.Utc).AddTicks(6756));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Products_ProductId",
+                table: "CartItems",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Products_ProductId",
+                table: "CartItems");
+
+            migrationBuilder.UpdateData(
+                table: "Warehouses",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 11, 25, 18, 15, 49, 888, DateTimeKind.Utc).AddTicks(8392));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Products_ProductId",
+                table: "CartItems",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
